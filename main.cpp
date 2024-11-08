8cfc256359efec2b979df642c10253a76489ca0e
Valentin Solntsev <solntse_valentin@mail.ru>
2024-11-08 13:45:43 +0300

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,63 +1,58 @@
 #include <iostream>
 #include <string>
 #include <cmath>
-#include <cstring>
 
 int main() {
-    double n, acc;
-    std::string o;
-    std::cin >> n;
+    double number;
+    double acc;
+    std::string operation;
     
+    std::cin >> number;
     if (std::cin.fail()) {
         std::cerr << "Error: Numeric operand expected" << std::endl;
     } else {
-        acc = n;
-
-        while (true) {
-            std::cin >> o;
-                       
-            if (o == "=") {
+        acc = number;
+        
+        do {
+            std::cin >> operation;
+            if (operation == "=") {
                 std::cout << acc << std::endl;
                 continue;
-            } else if (o == "q") {
-                break;
-            } else if (o == "c") {
+            } else if (operation == "c"){
                 acc = 0;
                 continue;
-            }
-            
-            std::cin >> n;
-            
+            } else if (operation == "q"){
+                break;
+            } 
+
+            std::cin >> number;
             if (std::cin.fail()) {
                 std::cerr << "Error: Numeric operand expected" << std::endl;
-                //std::cin.clear();
                 break;
             }
 
-            if (o == "+") {
-                acc += n;
-            } else if (o == "-") {
-                acc -= n;
-            } else if (o == "*") {
-                acc *= n;
-            } else if (o == "/") {
-                if (n == 0) {
-                    //std::cout << "inf" << std::endl;
+            if (operation == "+") {
+                acc += number;
+            } else if (operation == "-") {
+                acc -= number;
+            } else if (operation == "*") {
+                acc *= number;
+            } else if (operation == "/") {
+                if (number == 0) {
                     acc = std::numeric_limits<double>::infinity();
                 } else {
-                    acc /= n;
+                    acc /= number;   
                 }
-            } else if (o == "**") {
-                acc = std::pow(acc, n);
-            } else if (o == ":") {
-                acc = n;
-            } else if (o == "c") {
-                acc = 0;
+            } else if (operation == "**") {
+                acc = pow(acc, number);
+            } else if (operation == ":"){
+                acc = number;
             } else {
-                std::cerr << "Error: Unknown token " << o << std::endl;
-                //std::cin.clear();
+                std::cerr << "Error: Unknown token " << operation << std::endl;
                 break;
             }
-        }
+
+        } while(true);
     }
 }
+
